fcdeb3201b73716248f2e7654e5a1867d54e3dd3
Jovan <108335055+Esotis@users.noreply.github.com>
2024-04-29 15:30:38 +0700

    Update mongoose.js

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://Jovan:jovan123@clustertesting.5iaxchf.mongodb.net/testing?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://YourAccount:YourPassword@YourCluster.5iaxchf.mongodb.net/testing?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
